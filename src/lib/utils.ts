b28896e1788f71e28ad5f4c18decf746b4a9452b
John Cornyn <JMIHC@users.noreply.github.com>
2025-04-26 13:37:08 -0700

    fix commit

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,10 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+/**
+ * Combines multiple class names into a single string, resolving
+ * Tailwind CSS conflicts using twMerge.
+ */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
